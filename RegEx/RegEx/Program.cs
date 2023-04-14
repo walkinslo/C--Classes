9fb93dc5996d6b5d5730118651fa17fecbe71031
Walkinslo <mark.tkachev.1995@gmail.com>
2023-04-14 11:42:23 +0300

    Pr11

--- RegEx/RegEx/Program.cs
+++ RegEx/RegEx/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿// Ввести текст. Определить, является ли он текстом на русском языке. 
+using System.Text.RegularExpressions;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите текст: ");
+        string text = Console.ReadLine();
+
+        if (CheckText(text))
+        {
+            Console.WriteLine("Строка написана на русском");
+        } else
+        {
+            Console.WriteLine("Строка не содержит русские буквы");
+        }
+    }
+
+    static bool CheckText(string text)
+    {
+        Regex regex = new Regex(@"[\u0400-\u04FF]+");
+        return regex.IsMatch(text);
+    }
+}
