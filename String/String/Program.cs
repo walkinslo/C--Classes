8f1d2a18af13221dc8c60f3fe20868b474ba633c
Walkinslo <mark.tkachev.1995@gmail.com>
2023-04-14 10:50:01 +0300

    initial commit

---
+++ String/String/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите текст: ");
+        string input = Console.ReadLine();
+        string output = FormatText(input);
+        Console.WriteLine("Отформатированный текст: ");
+        Console.WriteLine(output);
+    }
+
+    static string FormatText(string text)
+    {
+        string[] sentences = text.Split('.');
+        string output = "";
+        foreach (string sentence in sentences)
+        {
+            // Это без пробелов
+            string trimmed = sentence.Trim();
+
+            if (trimmed.Length > 0)
+            {
+                // Тут мы делаем первую букву заглавной
+                char[] charArray = trimmed.ToCharArray();
+                charArray[0] = char.ToUpper(charArray[0]);
+                string capitalized = new string(charArray);
+
+                // Добавляем пробелы после точек
+                output += capitalized + ". ";
+
+                if (sentence.Length > trimmed.Length)
+                {
+                    output += sentence.Substring(trimmed.Length, sentence.Length - trimmed.Length);
+                }
+            }
+        }
+        return output;
+    }
+}
