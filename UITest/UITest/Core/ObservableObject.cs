b6ff6d3536ce0e0e7bce4908e1aacf697be0864f
unknown <mark.tkachev.1995@gmail.com>
2023-10-15 20:47:07 +0300

    Added UI

---
+++ UITest/UITest/Core/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UITest.Core
+{
+    internal class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPtopertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
